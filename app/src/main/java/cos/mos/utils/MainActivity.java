4ffdf5ed812e967e867022b44cbac7466718f6f4
Kosmos <KosmoSakura@gmail.com>
2018-11-13 20:42:25 +0800

    666

--- app/src/main/java/cos/mos/utils/MainActivity.java
+++ app/src/main/java/cos/mos/utils/MainActivity.java
@@ -1,13 +1,20 @@
 package cos.mos.utils;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import cos.mos.utils.init.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+    @Override
+    protected int layout() {
+        return R.layout.activity_main;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void init() {
+
+    }
+
+    @Override
+    protected void logic() {
+
     }
 }

a0801c2607210ee29b4f67531fbc362e2fcad947
Kosmos <KosmoSakura@gmail.com>
2018-12-17 15:41:41 +0800

    应用程序工具包

--- kosmoslibrary/src/main/java/cos/mos/library/utils/UIntent.java
+++ kosmoslibrary/src/main/java/cos/mos/library/utils/UIntent.java
@@ -1,10 +1,12 @@
 package cos.mos.library.utils;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.Settings;
 
 import cos.mos.library.init.KApp;
+
 
 /**
  * @Description: 各中页面跳转
@@ -28,9 +30,20 @@ public class UIntent {
      * 去系统授权页面（有权限查看使用情况的应用）
      * 高级权限
      */
-    public static void goSystemAdvanced() {
+    public static void goSysAdvanced() {
         Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
         KApp.getInstance().startActivity(intent);
+    }
+
+    /**
+     * @param activity 页面
+     * @param request  请求值
+     * @apiNote 去系统授权页面（有权限查看使用情况的应用）
+     * * 高级权限-带返回值
+     */
+    public static void goSysAdvanced(Activity activity, int request) {
+        Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
+        activity.startActivityForResult(intent, request);
     }
 
     /**
@@ -40,5 +53,14 @@ public class UIntent {
         Intent homeIntent = new Intent(Intent.ACTION_MAIN);
         homeIntent.addCategory(Intent.CATEGORY_HOME);
         KApp.getInstance().startActivity(homeIntent);
+    }
+
+    /**
+     * 去无障碍授权页面
+     */
+    public static void goAssist() {
+        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        KApp.getInstance().startActivity(intent);
     }
 }

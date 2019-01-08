5f8ed7a548a8388b02f4a9e216d532d2f66e216a
Kosmos <KosmoSakura@gmail.com>
2019-01-08 16:37:16 +0800

    单位转换，保留两位小数

--- kosmoslibrary/src/main/java/cos/mos/library/utils/UUnit.java
+++ kosmoslibrary/src/main/java/cos/mos/library/utils/UUnit.java
@@ -1,6 +1,6 @@
 package cos.mos.library.utils;
 
-import java.math.BigDecimal;
+import java.util.Locale;
 
 import cos.mos.library.init.KApp;
 
@@ -22,26 +22,53 @@ public class UUnit {
     public static String sizeFormatbit(double size) {
         double kiloByte = size / 1024;
         if (kiloByte < 1) {
-            return size + "\tByte";
+            return fotmat(size) + "\tB";
         }
         double megaByte = kiloByte / 1024;
         if (megaByte < 1) {
-            BigDecimal result1 = new BigDecimal(Double.toString(kiloByte));
-            return result1.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "\tKB";
+            return fotmat(kiloByte) + "\tKB";
         }
         double gigaByte = megaByte / 1024;
         if (gigaByte < 1) {
-            BigDecimal result2 = new BigDecimal(Double.toString(megaByte));
-            return result2.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "\tMB";
+            return fotmat(megaByte) + "\tMB";
         }
         double teraBytes = gigaByte / 1024;
         if (teraBytes < 1) {
-            BigDecimal result3 = new BigDecimal(Double.toString(gigaByte));
-            return result3.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "\tGB";
+            return fotmat(gigaByte) + "\tGB";
         }
-        BigDecimal result4 = new BigDecimal(teraBytes);
-        return result4.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString() + "\tTB";
+        return fotmat(teraBytes) + "\tTB";
     }
+
+    /**
+     * @apiNote 保留两位小数
+     * *方式一
+     * DecimalFormat df1 = new DecimalFormat("0.00");
+     * String str = df1.format(num);
+     * System.out.println(str);  //13.15
+     * *方式二:#.00 表示两位小数 #.0000四位小数
+     * DecimalFormat df2 =new DecimalFormat("#.00");
+     * String str2 =df2.format(num);
+     * System.out.println(str2);  //13.15
+     * *方式三
+     * %.2f 其中：%→表示 小数点前任意位数   2→表示两位小数 格式后的结果为f→表示浮点型
+     * String result = String.format("%.2f", num);
+     * System.out.println(result);  //13.15
+     * *方式四
+     * BigDecimal bigDecimal = new BigDecimal(Double.toString(digit));
+     * bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP).toPlainString();
+     * setScale(1)→保留一位小数，默认四舍五入方式
+     * setScale(1,BigDecimal.ROUND_DOWN)→直接删除多余的小数位，如2.35会变成2.3
+     * setScale(1,BigDecimal.ROUND_UP)→进位处理，2.35变成2.4
+     * setScale(1,BigDecimal.ROUND_HALF_UP)→四舍五入，2.35变成2.4
+     * setScaler(1,BigDecimal.ROUND_HALF_DOWN)→四舍五入，2.35变成2.3，如果是5则向下舍
+     * setScaler(1,BigDecimal.ROUND_CEILING)→接近正无穷大的舍入
+     * setScaler(1,BigDecimal.ROUND_FLOOR)→接近负无穷大的舍入，数字>0和ROUND_UP作用一样，数字<0和ROUND_DOWN作用一样
+     * setScaler(1,BigDecimal.ROUND_HALF_EVEN)→向最接近的数字舍入，如果与两个相邻数字的距离相等，则向相邻的偶数舍入。
+     */
+    private static String fotmat(double digit) {
+        return String.format(Locale.CHINA, "%.2f", digit);
+    }
+
 
     /**
      * @param version    要比较的版本号

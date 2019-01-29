85fec977460bda7c7369fe77ad9e7d93e8315fde
Kosmos <KosmoSakura@gmail.com>
2019-01-29 19:17:08 +0800

    关于A拖到B位置的处理

--- app/src/main/java/cos/mos/utils/ui/DragTip.java
+++ app/src/main/java/cos/mos/utils/ui/DragTip.java
@@ -4,7 +4,6 @@ import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
 
-import cos.mos.utils.R;
 import cos.mos.utils.init.BaseActivity;
 
 
@@ -15,6 +14,7 @@ import cos.mos.utils.init.BaseActivity;
  */
 public class DragTip extends BaseActivity {
     private boolean inRight = false;
+    private int id1, id2;
 
     @Override
     protected int layout() {
@@ -28,43 +28,40 @@ public class DragTip extends BaseActivity {
 
     @Override
     protected void logic() {
-        findViewById(R.id.lv13_fruit).setOnTouchListener((v, event) -> {
+        findViewById(id1).setOnTouchListener((v, event) -> {
             if (event.getAction() == MotionEvent.ACTION_DOWN) {
                 v.performClick();
                 v.startDrag(null, new View.DragShadowBuilder(v), null, 0);
             }
             return false;
         });
-        findViewById(R.id.lv13_i).setOnDragListener((v, event) -> {
+        findViewById(id2).setOnDragListener((v, event) -> {
             switch (event.getAction()) {
-                switch (event.getAction()) {
-                    case DragEvent.ACTION_DRAG_STARTED:
-                        // 拖拽开始:
-                        return true;
-                    case DragEvent.ACTION_DRAG_ENTERED:
-                        // 被拖拽View进入目标区域:
-                        inRight = true;
-                        return true;
-                    case DragEvent.ACTION_DRAG_LOCATION:
-                        // 被拖拽View在目标区域移动:
-                        inRight = true;
-                        return true;
-                    case DragEvent.ACTION_DRAG_EXITED:
-                        // 被拖拽View离开目标区域:
-                        inRight = false;
-                        return true;
-                    case DragEvent.ACTION_DROP:
-                        // 放开被拖拽View
-                        if (inRight) {
-                            //doSth
-                        }
-                        return true;
-                    case DragEvent.ACTION_DRAG_ENDED:
-                        // 拖拽完成
-                        return true;
-                }
+                case DragEvent.ACTION_DRAG_STARTED:
+                    // 拖拽开始:
+                    return true;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    // 被拖拽View进入目标区域:
+                    inRight = true;
+                    return true;
+                case DragEvent.ACTION_DRAG_LOCATION:
+                    // 被拖拽View在目标区域移动:
+                    inRight = true;
+                    return true;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    // 被拖拽View离开目标区域:
+                    inRight = false;
+                    return true;
+                case DragEvent.ACTION_DROP:
+                    // 放开被拖拽View
+                    if (inRight) {
+                        //doSth
+                    }
+                    return true;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    // 拖拽完成
+                    return true;
             }
-
             return false;
         });
     }

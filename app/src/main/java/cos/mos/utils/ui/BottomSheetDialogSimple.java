01cfc36994d76865bbd6046885f9603a8ea4d25b
Kosmos <KosmoSakura@gmail.com>
2019-03-19 18:04:27 +0800

    BottomSheetDialog滑动冲突

--- app/src/main/java/cos/mos/utils/ui/BottomSheetDialogSimple.java
+++ app/src/main/java/cos/mos/utils/ui/BottomSheetDialogSimple.java
@@ -1,11 +1,20 @@
 package cos.mos.utils.ui;
 
-import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.BottomSheetDialog;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ListView;
+
+import java.util.List;
 
 import cos.mos.utils.R;
+import cos.mos.utils.constant.AppBean;
 import cos.mos.utils.init.k.KActivity;
+import cos.mos.utils.ui.data.IgnoreAdapter;
+import cos.mos.utils.utils.system.UApps;
+import cos.mos.utils.utils.system.UScreen;
 
 /**
  * @Description: <p>
@@ -31,11 +40,87 @@ public class BottomSheetDialogSimple extends KActivity {
 
     private BottomSheetDialog diaBottom;
 
-    private void showDialog(Bitmap bmp) {
+    /**
+     * 最简单的用法
+     */
+    private void showDialog1() {
         if (diaBottom == null) {
             View view = View.inflate(context, R.layout.dia_wifi_code, null);
             diaBottom = new BottomSheetDialog(context, R.style.TransparentBottomSheetStyle);
             diaBottom.setContentView(view);
+        }
+        if (diaBottom.isShowing()) {
+            diaBottom.hide();
+        } else {
+            diaBottom.show();
+        }
+    }
+
+    /**
+     * BottomSheetBehavior的作用演示
+     * STATE_HIDDEN: 隐藏状态。默认是false，可通过app:behavior_hideable属性设置。
+     * STATE_COLLAPSED: 折叠关闭状态。可通过app:behavior_peekHeight来设置显示的高度,peekHeight默认是0。
+     * STATE_DRAGGING: 被拖拽状态
+     * STATE_SETTLING: 拖拽松开之后到达终点位置（collapsed or expanded）前的状态。
+     * STATE_EXPANDED: 完全展开的状态。
+     */
+    private void showDialog2() {
+        if (diaBottom == null) {
+            View view = View.inflate(context, R.layout.dia_wifi_code, null);
+            diaBottom = new BottomSheetDialog(context, R.style.TransparentBottomSheetStyle);
+            diaBottom.setContentView(view);
+            //调用setContentView方法之后
+            BottomSheetBehavior behavior = BottomSheetBehavior.from((View) view.getParent());
+            behavior.setPeekHeight(UScreen.dp2px(500));//手动设置打开高度
+            behavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+                @Override
+                public void onStateChanged(@NonNull View view, int newState) {
+                    //用户滑动关闭BottomSheetDialog
+                    if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                        diaBottom.dismiss();
+                        behavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    }
+                }
+
+                @Override
+                public void onSlide(@NonNull View view, float v) {
+
+                }
+            });
+        }
+        if (diaBottom.isShowing()) {
+            diaBottom.hide();
+        } else {
+            diaBottom.show();
+        }
+    }
+
+    private IgnoreAdapter adapter;
+
+    /**
+     * BottomSheetDialog+Listview的滑动冲突问题
+     */
+    private void showDialog3() {
+        List<AppBean> list = UApps.instance().getAppList();
+        if (diaBottom == null) {
+            View view = View.inflate(context, R.layout.dia_wifi_code, null);
+            diaBottom = new BottomSheetDialog(context, R.style.TransparentBottomSheetStyle);
+            ListView listView = view.findViewById(R.id.ig_list);
+            adapter = new IgnoreAdapter(list, this);
+            listView.setAdapter(adapter);
+            diaBottom.setContentView(view);
+            listView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    if (!listView.canScrollVertically(-1)) {
+                        //canScrollVertically(-1)的值表示是否能向下滚动，false表示已经滚动到顶部
+                        listView.requestDisallowInterceptTouchEvent(false);
+                    } else {
+                        listView.requestDisallowInterceptTouchEvent(true);
+                    }
+                    return false;
+                }
+            });
         }
         if (diaBottom.isShowing()) {
             diaBottom.hide();
---
+++ app/src/main/java/cos/mos/utils/ui/data/IgnoreAdapter.java
@@ -0,0 +1,83 @@
+package cos.mos.utils.ui.data;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cos.mos.utils.R;
+import cos.mos.utils.constant.AppBean;
+import cos.mos.utils.utils.java.UText;
+
+/**
+ * @Description: <p>
+ * @Author: Kosmos
+ * @Date: 2019.03.19 16:51
+ * @Email: KosmoSakura@gmail.com
+ */
+public class IgnoreAdapter extends BaseAdapter {
+    private List<AppBean> list;
+    private Context context;
+
+    public IgnoreAdapter(List<AppBean> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    public void setData(List<AppBean> list) {
+        this.list = list;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public AppBean getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view;
+        ViewHolder holder;
+        if (convertView == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.item_app_list, parent, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        } else {
+            view = convertView;
+            holder = (ViewHolder) view.getTag();
+        }
+        AppBean app = list.get(position);
+        if (app != null) {
+            if (app.getImage() != null) {
+                holder.icon.setImageDrawable(app.getImage());
+            }
+            holder.tName.setText(UText.isNull(app.getAppName()));
+        }
+        return view;
+    }
+
+    private class ViewHolder {
+        final TextView tName;
+        final ImageView icon;
+
+        ViewHolder(View root) {
+            icon = root.findViewById(R.id.ignore_icon);
+            tName = root.findViewById(R.id.ignore_name);
+        }
+    }
+}

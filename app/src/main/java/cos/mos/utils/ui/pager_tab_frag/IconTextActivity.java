1df729631cf336670d3f774c0b3f8bf5445aa1bd
Kosmos <KosmoSakura@gmail.com>
2019-04-25 20:01:49 +0800

    TabLayout

--- app/src/main/java/cos/mos/utils/ui/pager_tab_frag/IconTextActivity.java
+++ app/src/main/java/cos/mos/utils/ui/pager_tab_frag/IconTextActivity.java
@@ -5,8 +5,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 
-import cos.mos.utils.R;
 import cos.mos.toolkit.init.KActivity;
+import cos.mos.utils.R;
 
 public class IconTextActivity extends KActivity {
     private TabLayout tl;
@@ -34,6 +34,8 @@ public class IconTextActivity extends KActivity {
         //预加载管理,除去当前显示页面以外需要被预加载的页面数。
         vp.setOffscreenPageLimit(2);
         tl.setupWithViewPager(vp);
+        tl.addTab(tl.newTab().setText("月"));
+        tl.addTab(tl.newTab().setText("年"));
         for (int i = 0; i < 3; i++) {
             TabLayout.Tab tab = tl.getTabAt(i);
             if (tab != null) {
@@ -44,6 +46,21 @@ public class IconTextActivity extends KActivity {
 
     @Override
     protected void logic() {
+        tl.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
 
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getPosition();
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }

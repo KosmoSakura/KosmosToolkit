4245a1a5a1c9ec8e108aaacd4697b537e7b30299
Kosmos <KosmoSakura@gmail.com>
2019-05-06 21:18:26 +0800

    okhttp网络 kotlin封装

--- app/src/main/java/cos/mos/utils/ukotlin/okhttp/KtNet.kt
+++ app/src/main/java/cos/mos/utils/ukotlin/okhttp/KtNet.kt
@@ -10,7 +10,7 @@ import java.lang.ref.WeakReference
  * @Author: Kosmos
  * @Date: 2019.05.06 17:29
  * @Email: KosmoSakura@gmail.com
- *  <uses-permission android:name="android.permission.INTERNET"/>
+ * <uses-permission android:name="android.permission.INTERNET"/>
  * <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
  */
 class KtNet private constructor() {

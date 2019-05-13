6504f9d96734e6d51f9de35b07b4fdda38b65038
Kosmos <KosmoSakura@gmail.com>
2019-05-13 17:37:05 +0800

    权限校验

--- app/src/main/java/cos/mos/utils/ui/SplashActivity.java
+++ app/src/main/java/cos/mos/utils/ui/SplashActivity.java
@@ -2,36 +2,53 @@ package cos.mos.utils.ui;
 
 import android.Manifest;
 import android.content.Intent;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.util.DisplayMetrics;
-
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.liaoinstan.springview.container.DefaultHeader;
-import com.liaoinstan.springview.widget.SpringView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import cos.mos.toolkit.system.UPermissions;
-import cos.mos.toolkit.ui.UDialog;
-import cos.mos.utils.R;
-import cos.mos.utils.mvp.bean.ImageBean;
-import cos.mos.utils.mvp.contract.KOnFreshListener;
-import cos.mos.utils.widget.list.AdapterImage;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        Handler().postDelayed({
+//            //检测权限
+//            checkPermissions(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//        }, 200)
+        checkPermissions(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+//        wayTwo();
+    }
 
+    private void checkPermissions(String... permissions) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            toNext();
+            return;
+        }
+        boolean st = true;
+        for (String str : permissions) {
+            if (PackageManager.PERMISSION_GRANTED != ContextCompat.checkSelfPermission(this, str)) {
+                st = false;
+                break;
+            }
+        }
+        if (st) {
+            toNext();
+        } else {
+            ActivityCompat.requestPermissions(this, permissions, 10086);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        checkPermissions(Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     private void toNext() {
@@ -39,25 +56,15 @@ public class SplashActivity extends AppCompatActivity {
         finish();
     }
 
-    @Override
-    protected void logic() {
+    private void wayTwo() {
         new UPermissions(this)
-            .check("我要权限，给我权限", new UPermissions.Listener() {
-
+            .check("理由", new UPermissions.Listener() {
                 @Override
                 public void permission(boolean hasPermission) {
-                    spv.callFreshDelay();
+                    if (hasPermission) {
+                        toNext();
+                    }
                 }
-            }, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA);
-        spv.setListener(new KOnFreshListener() {
-            @Override
-            public void onRefresh() {
-                presenter.getImageList(999);
-            }
-        });
-        adapterImage.setOnItemClickListener((adapter, view, position) -> {
-
-        });
+            }, Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE);
     }
-
 }

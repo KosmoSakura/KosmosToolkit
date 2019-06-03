12e39b9756199b77aae3ecab4a92044c586ee242
Kosmos <KosmoSakura@gmail.com>
2019-06-03 16:29:05 +0800

    refactoring2

--- KosmosToolkit/src/main/java/cos/mos/toolkit/init/KActivity.java
+++ KosmosToolkit/src/main/java/cos/mos/toolkit/init/KActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;

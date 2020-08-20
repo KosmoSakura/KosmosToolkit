17a214cbe0aad69ee6724a00e1d156bc3249eee5
Kosmos <KosmoSakura@gmail.com>
2020-08-20 18:16:42 +0800

    优化手势判断，小地图事件，和小地图显示时机

--- app/src/main/java/cos/mos/utils/widget/drag/ZoomView.kt
+++ app/src/main/java/cos/mos/utils/widget/drag/ZoomView.kt
@@ -15,6 +15,7 @@ import kotlin.math.sign
  * @Author Kosmos
  * @Date 2020.03.25 00:18
  * @Email KosmoSakura@gmail.com
+ * @tip 2020.8.20 优化手势判断，小地图事件，和小地图显示时机
  * */
 class ZoomView : FrameLayout {
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
@@ -28,26 +29,28 @@ class ZoomView : FrameLayout {
         fun onZoomStarted(zoom: Float, zoomx: Float, zoomy: Float)
     }
 
-    // 坐标
-    private var zoom = 1.0f
-    private var doubleZoom = 2.0f//双倍缩放比例
-    private var tripleZoom = doubleZoom * 2//3倍缩放比例
-    private var quadraZoom = doubleZoom * 3 //4最大缩放比例
-    private var maxZoom = doubleZoom * 4 //最大缩放比例
-    private var smoothZoom = 1.0f
+    private val zoomOriginal = 1.0f //原始状态倍率
+    private val doubleZoom = 2f//双倍缩放比例
+    private val tripleZoom = 3f//2.5倍缩放比例
+    private val quadraZoom = 10f //4最大缩放比例
+    private val maxZoom = doubleZoom * 16 //最大缩放比例
+    private val minZoom = zoomOriginal //最小缩放比例,小于1即可在缩小
+
+    private var zoom = zoomOriginal//放大倍数
+    private var smoothZoom = zoomOriginal//当前放大倍数
     private var zoomX = 0f
     private var zoomY = 0f
     private var smoothZoomX = 0f//缩放中心点x
     private var smoothZoomY = 0f//缩放中心点y
     private var scrolling = false
 
-    // 视图变量
-    private var showMinimap = false
-    private var miniMapColor = Color.WHITE
-    private var miniMapHeight = -1
-    private var miniMapCaption: String? = null
-    private var miniMapCaptionSize = 10.0f
-    private var miniMapCaptionColor = Color.WHITE
+    // 小地图变量
+    private var showMinimap = true//小地图：总开关
+    private var miniMapColor = Color.GRAY//小地图：颜色
+    private var miniMapHeight = -1//小地图：高度
+    private var miniMapCaption: String? = "航海日志"//小地图：标题
+    private var miniMapCaptionSize = 32.0f//小地图：尺寸
+    private var miniMapCaptionColor = Color.WHITE//小地图：标题颜色
 
     // 触摸变量
     private var lastTapTime: Long = 0
@@ -79,7 +82,7 @@ class ZoomView : FrameLayout {
     }
 
     private fun smoothZoomTo(zoom: Float, x: Float, y: Float) {
-        smoothZoom = clamp(1.0f, zoom, maxZoom)
+        smoothZoom = clamp(minZoom, zoom, maxZoom)
         smoothZoomX = x
         smoothZoomY = y
         if (listener != null) {
@@ -93,28 +96,29 @@ class ZoomView : FrameLayout {
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
         if (ev.pointerCount == 1) {
-            processSingleTouchEvent(ev)//单击
+            processSingleTouchEvent(ev)//单点触控
         }
         if (ev.pointerCount == 2) {
-            processDoubleTouchEvent(ev)//双击
+            processDoubleTouchEvent(ev)//双点触控
         }
         rootView.invalidate() //重绘
         invalidate()
         return true
     }
 
-    //单击
+    //单点触控
     private fun processSingleTouchEvent(ev: MotionEvent) {
         val x = ev.x
         val y = ev.y
         val w = miniMapHeight * width.toFloat() / height
         val h = miniMapHeight.toFloat()
         val touchingMiniMap = x >= 10.0f && x <= 10.0f + w && y >= 10.0f && y <= 10.0f + h
-        if (showMinimap && smoothZoom > 1.0f && touchingMiniMap) {
+        if (showMinimap && smoothZoom > zoomOriginal && touchingMiniMap) {
             processSingleTouchOnMinimap(ev)
         } else processSingleTouchOutsideMinimap(ev)
     }
 
+    //小地图区域的单点触控
     private fun processSingleTouchOnMinimap(ev: MotionEvent) {
         val x = ev.x
         val y = ev.y
@@ -125,6 +129,7 @@ class ZoomView : FrameLayout {
         smoothZoomTo(smoothZoom, zx, zy)
     }
 
+    //小地图以外区域的单点触控
     private fun processSingleTouchOutsideMinimap(ev: MotionEvent) {
         val x = ev.x
         val y = ev.y
@@ -143,7 +148,7 @@ class ZoomView : FrameLayout {
                 touchLastY = y
                 scrolling = false
             }
-            MotionEvent.ACTION_MOVE -> if (scrolling || smoothZoom > 1.0f && l > 30.0f) {
+            MotionEvent.ACTION_MOVE -> if (scrolling || smoothZoom > zoomOriginal && l > 30.0f) {
                 if (!scrolling) {
                     scrolling = true
                     ev.action = MotionEvent.ACTION_CANCEL
@@ -157,10 +162,9 @@ class ZoomView : FrameLayout {
                 // 检查双击
                 if (System.currentTimeMillis() - lastTapTime < 500) {
                     when (smoothZoom) {
-                        1.0f -> smoothZoomTo(doubleZoom, x, y)
+                        zoomOriginal -> smoothZoomTo(doubleZoom, x, y)
                         doubleZoom -> smoothZoomTo(tripleZoom, x, y)
-                        tripleZoom -> smoothZoomTo(quadraZoom, x, y)
-                        else -> smoothZoomTo(1.0f, width / 2.0f, height / 2.0f)
+                        else -> smoothZoomTo(zoomOriginal, width / 2.0f, height / 2.0f)
                     }
                     lastTapTime = 0
                     ev.action = MotionEvent.ACTION_CANCEL
@@ -177,7 +181,7 @@ class ZoomView : FrameLayout {
         super.dispatchTouchEvent(ev)
     }
 
-    //双击
+    //双点触控
     private fun processDoubleTouchEvent(ev: MotionEvent) {
         val x1 = ev.getX(0)
         val dx1 = x1 - lastdx1
@@ -192,21 +196,21 @@ class ZoomView : FrameLayout {
         val dy2 = y2 - lastdy2
         lastdy2 = y2
         // 指针距离
-        val d = hypot(x2 - x1.toDouble(), y2 - y1.toDouble()).toFloat()
-        val dd = d - lastd
-        lastd = d
-        val ld = abs(d - startd)
+        val dist = hypot(x2 - x1.toDouble(), y2 - y1.toDouble()).toFloat()
+        val distDiff = dist - lastd//上次距离差
+        lastd = dist
+        val ld = abs(dist - startd)
         atan2(y2 - y1.toDouble(), x2 - x1.toDouble())
         when (ev.action) {
             MotionEvent.ACTION_DOWN -> {
-                startd = d
+                startd = dist
                 pinching = false
             }
             MotionEvent.ACTION_MOVE -> if (pinching || ld > 30.0f) {
                 pinching = true
                 val dxk = 0.5f * (dx1 + dx2)
                 val dyk = 0.5f * (dy1 + dy2)
-                smoothZoomTo(1.0f.coerceAtLeast(zoom * d / (d - dd)), zoomX - dxk / zoom, zoomY - dyk / zoom)
+                smoothZoomTo(minZoom.coerceAtLeast(zoom * dist / (dist - distDiff)), zoomX - dxk / zoom, zoomY - dyk / zoom)
             }
             MotionEvent.ACTION_UP -> pinching = false
             else -> pinching = false
@@ -215,8 +219,7 @@ class ZoomView : FrameLayout {
         super.dispatchTouchEvent(ev)
     }
 
-    private fun clamp(min: Float, value: Float, max: Float) =
-        min.coerceAtLeast(value.coerceAtMost(max))
+    private fun clamp(min: Float, value: Float, max: Float) = min.coerceAtLeast(value.coerceAtMost(max))
 
     private fun lerp(a: Float, b: Float, k: Float) = a + (b - a) * k
 
@@ -225,10 +228,8 @@ class ZoomView : FrameLayout {
     override fun dispatchDraw(canvas: Canvas) {
         // 开始缩放
         zoom = lerp(bias(zoom, smoothZoom, 0.05f), smoothZoom, 0.2f)
-        smoothZoomX =
-            clamp(0.5f * width / smoothZoom, smoothZoomX, width - 0.5f * width / smoothZoom)
-        smoothZoomY =
-            clamp(0.5f * height / smoothZoom, smoothZoomY, height - 0.5f * height / smoothZoom)
+        smoothZoomX = clamp(0.5f * width / smoothZoom, smoothZoomX, width - 0.5f * width / smoothZoom)
+        smoothZoomY = clamp(0.5f * height / smoothZoom, smoothZoomY, height - 0.5f * height / smoothZoom)
 
         zoomX = lerp(bias(zoomX, smoothZoomX, 0.1f), smoothZoomX, 0.35f)
         zoomY = lerp(bias(zoomY, smoothZoomY, 0.1f), smoothZoomY, 0.35f)
@@ -240,10 +241,8 @@ class ZoomView : FrameLayout {
         // 构建矩阵
         m.setTranslate(0.5f * width, 0.5f * height)
         m.preScale(zoom, zoom)
-        m.preTranslate(
-            -clamp(0.5f * width / zoom, zoomX, width - 0.5f * width / zoom),
-            -clamp(0.5f * height / zoom, zoomY, height - 0.5f * height / zoom)
-        )
+        m.preTranslate(-clamp(0.5f * width / zoom, zoomX, width - 0.5f * width / zoom),
+                -clamp(0.5f * height / zoom, zoomY, height - 0.5f * height / zoom))
         // 获取视图
         val v = getChildAt(0)
         m.preTranslate(v.left.toFloat(), v.top.toFloat())
@@ -264,32 +263,31 @@ class ZoomView : FrameLayout {
             v.draw(canvas)
             canvas.restore()
         }
-        // 绘制放大视图
-        if (showMinimap) {
+        // 绘制小地图
+        if (showMinimap && smoothZoom > zoomOriginal) {
             if (miniMapHeight < 0) {
                 miniMapHeight = height / 4
             }
-            canvas.translate(10.0f, 10.0f)
+            canvas.translate(10f, 10f)
             p.color = -0x80000000 or 0x00ffffff and miniMapColor
             val w = miniMapHeight * width.toFloat() / height
             val h = miniMapHeight.toFloat()
-            canvas.drawRect(0.0f, 0.0f, w, h, p)
+            canvas.drawRect(0.0f, 0.0f, w, h, p)//左上角
             if (miniMapCaption != null && miniMapCaption!!.isNotEmpty()) {
                 p.textSize = miniMapCaptionSize
                 p.color = miniMapCaptionColor
                 p.isAntiAlias = true
-                canvas.drawText(miniMapCaption!!, 10.0f, 10.0f + miniMapCaptionSize, p)
+                canvas.drawText(miniMapCaption!!, 10.0f, 10.0f + miniMapCaptionSize, p)//左上角
                 p.isAntiAlias = false
             }
             p.color = -0x80000000 or 0x00ffffff and miniMapColor
             val dx = w * zoomX / width
             val dy = h * zoomY / height
-            canvas.drawRect(dx - 0.5f * w / zoom, dy - 0.5f * h / zoom, dx + 0.5f * w / zoom, dy + 0.5f * h / zoom, p)
-            canvas.translate(-10.0f, -10.0f)
+            canvas.drawRect(dx - 0.5f * w / zoom, dy - 0.5f * h / zoom, dx + 0.5f * w / zoom, dy + 0.5f * h / zoom, p)//左上角
+            canvas.translate(-10f, -10f)
         }
         // 重绘
         rootView.invalidate()
         invalidate()
     }
-
 }

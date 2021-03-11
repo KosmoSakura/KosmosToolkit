2a00daee23ee496a4a460d4d0ca22a6bf4603f10
Kosmos <KosmoSakura@gmail.com>
2021-03-11 18:45:58 +0800

    小地图标题

--- app/src/main/java/cos/mos/utils/widget/drag/ZoomView.kt
+++ app/src/main/java/cos/mos/utils/widget/drag/ZoomView.kt
@@ -18,6 +18,7 @@ import kotlin.math.sign
  * @tip 2020.8.20 优化手势判断，小地图事件，和小地图显示时机
  * @tip 2020.12.29 缩放重置
  * @tip 2021.1.10 小地图开关
+ * @tip 2021.3.11 小地图标题
  * */
 class ZoomView : FrameLayout {
     constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
@@ -75,6 +76,10 @@ class ZoomView : FrameLayout {
     // 监听
     private var listener: ZoomViewListener? = null
     private var ch: Bitmap? = null
+
+    fun setMinimapTitle(Caption: String?) {
+        miniMapCaption = Caption
+    }
 
     //小地图开关
     fun showMinimap(show: Boolean) {
